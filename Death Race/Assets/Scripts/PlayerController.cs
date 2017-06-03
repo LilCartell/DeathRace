8c33fb1625389a806b246ce871d168b7e73b5fba
Ismuur <aymeric.lambolez@epitech.eu>
2017-06-03 17:44:38 +0200

    start bebe animations

--- Death Race/Assets/Scripts/PlayerController.cs
+++ Death Race/Assets/Scripts/PlayerController.cs
@@ -12,12 +12,16 @@ public class PlayerController : MonoBehaviour {
     public float groundCheckRadius = 0.1f;
     public float groundCheckPositionOffset = -0.5f;
 
+    private Animator _animator;
+    private bool jump;
+    private bool _walk;
+
     Rigidbody2D playerRigidbody;
     // Use this for initialization
     void Start () {
         playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        _animator = gameObject.GetComponent<Animator>();
 	}
-    bool jump;
         
         // Update is called once per frame
     void Update () {
@@ -26,12 +30,26 @@ public class PlayerController : MonoBehaviour {
         pos.y += groundCheckPositionOffset;
         bool grounded = Physics2D.OverlapCircleAll(pos, groundCheckRadius).Length > 1;
         bool lastJump = jump;
+        bool lastWalk = _walk;
 
         if (grounded)
             playerRigidbody.velocity = new Vector2(move * maxSpeed, playerRigidbody.velocity.y);
         else if (!grounded && (move >= 0.5f || move <= -0.5f))
             playerRigidbody.velocity = new Vector2(move * Mathf.Min(Mathf.Abs(playerRigidbody.velocity.x + (move * inAirSpeed)), maxSpeed), playerRigidbody.velocity.y);
         jump = Input.GetButton("Jump" + playerIndex.ToString());
+        _walk = grounded && (move >= 0.1f || move <= -0.1f);
+        if (_walk != lastWalk)
+        {
+            if (_walk)
+                print("Start walking");
+            else
+                print("Stop walking");
+
+            if (_walk)
+                _animator.SetTrigger("StartWalk");
+            else
+                _animator.SetTrigger("StopWalk");
+        }
 
         if (lastJump != jump && jump && grounded)
         {
