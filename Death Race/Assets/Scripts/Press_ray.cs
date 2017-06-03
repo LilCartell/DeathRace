371f5705f51f2c7fae6bd447b70409e3718eafe7
Youri <youri.gicquel@speedernet.fr>
2017-06-03 17:10:35 +0200

    Press

---
+++ Death Race/Assets/Scripts/Press_ray.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Press_ray : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && !transform.parent.gameObject.GetComponent<Rigidbody2D>())
+        {
+            transform.parent.gameObject.AddComponent<Rigidbody2D>();
+        }
+    }
+}
